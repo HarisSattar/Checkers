15395a27ece13f1b6d1753fabf064043b6b461ba
root <harissattar41@gmail.com>
2018-01-26 21:16:40 -0800

    init commit

---
+++ main.lua
@@ -0,0 +1,11 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+local width = display.contentWidth
+local background = display.newImageRect("board.png", width, width)
+background.x = display.contentCenterX
+background.y = display.contentCenterY
